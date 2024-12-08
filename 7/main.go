c4dda916ab01b2450735d465d8eb5a290e86d858
Brian Brenner <bbrenner321@gmail.com>
2024-12-07 23:51:04 -0500

    cleaner now

--- 7/main.go
+++ 7/main.go
@@ -7,26 +7,14 @@ import (
 	"strings"
 )
 
-// TODO: cleanup and simplify
-
-func helper(target int, nums []int, i int, cur int, op string) bool {
+func helper(target int, nums []int, i int, cur int, op string, validOps []string) bool {
+	if cur > target {
+		return false
+	}
 	if i == len(nums) {
 		return cur == target
 	}
-	if op == "+" {
-		cur += nums[i]
-	} else if op == "*" {
-		cur *= nums[i]
-	} else {
-		fmt.Println("bad")
-	}
-	return helper(target, nums, i+1, cur, "*") || helper(target, nums, i+1, cur, "+")
-}
 
-func helper2(target int, nums []int, i int, cur int, op string) bool {
-	if i == len(nums) {
-		return cur == target
-	}
 	if op == "+" {
 		cur += nums[i]
 	} else if op == "*" {
@@ -34,41 +22,14 @@ func helper2(target int, nums []int, i int, cur int, op string) bool {
 	} else if op == "||" {
 		cur, _ = strconv.Atoi(strconv.Itoa(cur) + strconv.Itoa(nums[i]))
 	} else {
-		fmt.Println("bad")
-	}
-	return helper2(target, nums, i+1, cur, "*") || helper2(target, nums, i+1, cur, "+") || helper2(target, nums, i+1, cur, "||")
-}
-
-func getScore(line string) int {
-	split := strings.Split(line, ": ")
-	target, _ := strconv.Atoi(split[0])
-	numsRaw := strings.Split(split[1], " ")
-	nums := []int{}
-	for _, n := range numsRaw {
-		num, _ := strconv.Atoi(n)
-		nums = append(nums, num)
+		fmt.Println("bad input")
 	}
 
-	if helper(target, nums, 1, nums[0], "+") || helper(target, nums, 1, nums[0], "*") {
-		return target
+	out := false
+	for _, nextOp := range validOps {
+		out = out || helper(target, nums, i+1, cur, nextOp, validOps)
 	}
-	return 0
-}
-
-func getScore2(line string) int {
-	split := strings.Split(line, ": ")
-	target, _ := strconv.Atoi(split[0])
-	numsRaw := strings.Split(split[1], " ")
-	nums := []int{}
-	for _, n := range numsRaw {
-		num, _ := strconv.Atoi(n)
-		nums = append(nums, num)
-	}
-
-	if helper2(target, nums, 1, nums[0], "+") || helper2(target, nums, 1, nums[0], "*") || helper2(target, nums, 1, nums[0], "||") {
-		return target
-	}
-	return 0
+	return out
 }
 
 func part1() {
@@ -79,7 +40,20 @@ func part1() {
 
 	tot := 0
 	for _, line := range lines {
-		tot += getScore(line)
+		split := strings.Split(line, ": ")
+		target, _ := strconv.Atoi(split[0])
+		numsRaw := strings.Split(split[1], " ")
+		nums := []int{}
+		for _, n := range numsRaw {
+			num, _ := strconv.Atoi(n)
+			nums = append(nums, num)
+		}
+
+		validOps := []string{"+", "*"}
+		if helper(target, nums, 1, nums[0], "+", validOps) || helper(target, nums, 1, nums[0], "*", validOps) {
+			tot += target
+		}
+
 	}
 	fmt.Println(tot)
 }
@@ -92,7 +66,19 @@ func part2() {
 
 	tot := 0
 	for _, line := range lines {
-		tot += getScore2(line)
+		split := strings.Split(line, ": ")
+		target, _ := strconv.Atoi(split[0])
+		numsRaw := strings.Split(split[1], " ")
+		nums := []int{}
+		for _, n := range numsRaw {
+			num, _ := strconv.Atoi(n)
+			nums = append(nums, num)
+		}
+
+		validOps := []string{"+", "*", "||"}
+		if helper(target, nums, 1, nums[0], "+", validOps) || helper(target, nums, 1, nums[0], "*", validOps) || helper(target, nums, 1, nums[0], "||", validOps) {
+			tot += target
+		}
 	}
 	fmt.Println(tot)
 }
