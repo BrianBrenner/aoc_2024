76159369119a7223a2875c0d58f224c14649b4b5
Brian Brenner <bbrenner321@gmail.com>
2024-12-26 12:05:05 -0500

    day 16 checkpoint, works for test but too slow for real input

--- 16/main.go
+++ 16/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -49,6 +50,42 @@ func cloneVisited(v [][]bool) [][]bool {
 	return cloned
 }
 
+func dfs(grid [][]string, cur Step, finishes *[]int) {
+	// reached end
+	if grid[cur.R][cur.C] == "E" {
+		*finishes = append(*finishes, cur.Points)
+	}
+
+	for _, dir := range dirs {
+		nextR := cur.R + int(imag(dir))
+		nextC := cur.C + int(real(dir))
+		// in bounds
+		if nextR >= 0 && nextR < len(grid) && nextC >= 0 && nextC < len(grid) {
+			// valid and not visited
+			if grid[nextR][nextC] != "#" && !(*cur.Visited)[nextR][nextC] {
+				nextVisited := cloneVisited(*cur.Visited)
+				nextVisited[nextR][nextC] = true
+				nextPoints := cur.Points + 1
+				// i think we don't have to worry about turning 180 degrees, because that would have been visited
+				if cur.Dir != dir {
+					nextPoints = cur.Points + 1001
+				}
+				next := Step{
+					R:       nextR,
+					C:       nextC,
+					Dir:     dir,
+					Points:  nextPoints,
+					Visited: &nextVisited,
+				}
+				dfs(grid, next, finishes)
+			}
+		}
+	}
+}
+
+// TODO: maybe do a weighted bfs? like if you have to turn, put it at the back of the queue, but if you are going
+// straight put at the front?
+
 // i think maybe use dfs, or seperate visited array for each iteration. maybe look at day 6
 func bfs(grid [][]string, startR, startC int) int {
 	q := []Step{}
@@ -63,18 +100,19 @@ func bfs(grid [][]string, startR, startC int) int {
 	}
 	q = append(q, start)
 
-	minPoints := -1
+	//minPoints := -1
 	for len(q) > 0 {
 		cur := q[0]
 		q = q[1:]
 		// reached end
 		if grid[cur.R][cur.C] == "E" {
-			if minPoints == -1 {
-				minPoints = cur.Points
-			} else if cur.Points < minPoints {
-				minPoints = cur.Points
-			}
-			continue
+			return cur.Points
+			//if minPoints == -1 {
+			//	minPoints = cur.Points
+			//} else if cur.Points < minPoints {
+			//	minPoints = cur.Points
+			//}
+			//continue
 		}
 		for _, dir := range dirs {
 			nextR := cur.R + int(imag(dir))
@@ -85,30 +123,47 @@ func bfs(grid [][]string, startR, startC int) int {
 				if grid[nextR][nextC] != "#" && !(*cur.Visited)[nextR][nextC] {
 					nextVisited := cloneVisited(*cur.Visited)
 					nextVisited[nextR][nextC] = true
-					nextPoints := cur.Points + 1
-					// i think we don't have to worry about turning 180 degrees, because that would have been visited
-					if cur.Dir != dir {
-						nextPoints = cur.Points + 1001
-					}
 					next := Step{
 						R:       nextR,
 						C:       nextC,
 						Dir:     dir,
-						Points:  nextPoints,
 						Visited: &nextVisited,
 					}
-					q = append(q, next)
+
+					// i think we don't have to worry about turning 180 degrees, because that would have been visited
+					if cur.Dir != dir {
+						next.Points = cur.Points + 1001
+						q = append(q, next)
+					} else {
+						next.Points = cur.Points + 1
+						q = append([]Step{next}, q...)
+					}
 				}
 			}
 		}
 	}
 
-	return minPoints
+	return -1
+}
+
+func helper(grid [][]string, startR, startC int) int {
+	v := createVisited(len(grid), len(grid[0]))
+	v[startR][startC] = true
+	start := Step{
+		R:       startR,
+		C:       startC,
+		Dir:     complex(1, 0),
+		Points:  0,
+		Visited: &v,
+	}
+	finishes := []int{}
+	dfs(grid, start, &finishes)
+	return slices.Min(finishes)
 }
 
 func part1() {
-	raw, _ := os.ReadFile("test.txt")
-	//raw, _ := os.ReadFile("input.txt")
+	//raw, _ := os.ReadFile("test.txt")
+	raw, _ := os.ReadFile("input.txt")
 	data := strings.Split(string(raw), "\n")
 	grid := [][]string{}
 	for _, line := range data {
@@ -132,6 +187,7 @@ func part1() {
 	}
 
 	fmt.Println(bfs(grid, r, c))
+	//fmt.Println(helper(grid, r, c))
 }
 
 func main() {
